fc3fa6be5c4ee60283554ef760ef3ac2a2e63096
unknown <erandirsm13@gmail.com>
2023-12-13 13:50:27 -0300

    portfolio using html, css and javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".contact-btn");
+
+const contact = () => {
+  window.location.href =
+    "https://mail.google.com/mail/u/0/#inbox?compose=CllgCJNvNQlkvlgckfLQcrCTBNjCkpPcnvnvfSCKbjlccwxKMKKDDCMdDZfXqLDwVMKPmblKtTL";
+}
+
+btn.addEventListener('click', contact);
